9713d44c8ce602fa6c62b522289a6afea9f16737
Cameron Goode <cgoode101@gmail.com>
2020-06-05 16:48:15 -0400

    get playlists from the server

--- client/src/components/Playlist.js
+++ client/src/components/Playlist.js
@@ -5,11 +5,14 @@ import {Card} from 'react-bootstrap';
  * @props playlist - a playlist object
  */
 
+// TODO: Update the playlist display.
+
 const Playlist = (props) => {
     return (
         <Card>
             <Card.Body>
-                <h2>{props.playlist.name}</h2>
+                <h2>{props.playlist.song}</h2>
+                <h4>{props.playlist.artist}</h4>
             </Card.Body>
         </Card>
     )

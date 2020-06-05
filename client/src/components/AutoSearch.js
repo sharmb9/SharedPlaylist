e81e52abe1f6f1dd740060648d65ffc9254361bf
Sharma Bilaval <sharmb9@mcmaster.ca>
2020-06-05 18:09:42 -0400

    done autosearch, gotta fix rendering

--- client/src/components/AutoSearch.js
+++ client/src/components/AutoSearch.js
@@ -1,12 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search";
 import SpotifyWebApi from "spotify-web-api-js";
 
 const spotifyApi = new SpotifyWebApi();
 
 const AutoSearch = () => {
-
-     /**
+  /**
    * Obtains parameters from the hash of the URL
    * @return Object
    */
@@ -22,29 +21,47 @@ const AutoSearch = () => {
   };
 
   const [songsState, setSongsState] = useState({
-    // setting default search query as string a for now(change to later)
-    searchQuery: "a",
-    suggestedSongs: null,
+    suggestedSongs: [],
   });
+  const { suggestedSongs } = songsState;
 
-  const { searchQuery, suggestedSongs } = songsState;
+  const [searchQueryState, setSearchQueryState] = useState({
+    searchQuery: " ",
+  });
+  const { searchQuery } = searchQueryState;
 
-  const onSearchChange = async (e) => {
-    setSongsState({ ...songsState, searchQuery: e.target.value });
-    if (getHashParams().access_token) {
-        // console.log(getHashParams().access_token)
-        spotifyApi.setAccessToken(getHashParams().access_token);
-      }
-    try {
-        const res = await spotifyApi.search(searchQuery,["track"])
-        console.log(res.tracks.items.map( (item) => item.name))
-      } catch (error) {
-          console.log(error.message)
-      }
+  const onSearchChange = (e) => {
+    setSearchQueryState({ searchQuery: e.target.value });
+    // getSearchedTracks(searchQuery);
   };
 
-  const getSongs = async () => {
+  useEffect(() => {
+    // getSearchedTracks(searchQuery)
+    const getSearchedTracks = async (searchQuery) => {
+      if (getHashParams().access_token) {
+        spotifyApi.setAccessToken(getHashParams().access_token);
+      }
+      try {
+        const res = await spotifyApi.search(searchQuery, ["track"]);
+        const songs = res.tracks.items.map((item) => item.name);
+        console.log(songs);
+        setSuggestions(songs);
+        // suggestions based on user input
+        // const filteredSongs = suggestedSongs.toLowerCase().filter((songs) => searchQuery.toLowerCase() > -1);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getSearchedTracks(searchQuery)
+  }, []);
 
+  const setSuggestions = (trackArray) => {
+    const newSongState = {
+      suggestedSongs: trackArray,
+    };
+    setSongsState({ newSongState });
+    // console.log(suggestedSongs);
+    // setSongsState({...songsState, suggestedSongs: trackArray });
   };
 
   return (
@@ -55,6 +72,9 @@ const AutoSearch = () => {
         placeholder="Search a song..."
         name="searchQuery"
       />
+      {/* {suggestedSongs.map((song) => (
+        <p>{song}</p>
+      ))} */}
     </div>
   );
 };

2924f967b236eb8df4a750a8a80a589d29ec62ad
Sharma Bilaval <sharmb9@mcmaster.ca>
2020-06-12 05:22:55 -0400

    Gets hashparams from util function

--- client/src/components/PlaylistDisplay.jsx
+++ client/src/components/PlaylistDisplay.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Playlist from './Playlist';
-import { getHashParams } from './AutoSearch';
+import React from "react";
+import { Link } from "react-router-dom";
+import Playlist from "./Playlist";
+import {getHashParams} from "./util/spotify";
+
 
 /**
  * The playlist display is the container for all of the playlist cards.
@@ -10,15 +11,24 @@ import { getHashParams } from './AutoSearch';
 
 const PlaylistDisplay = (props) => {
   const { playlists } = props;
+  const params = getHashParams();
+  let access_token = params.access_token;
   return (
     <div>
-      {playlists !== []
-        ? playlists.map((playlist, index) => (
-          <Link key={index.toString()} to={`/playlist/${playlist.title}#access_token=${getHashParams().access_token}`}>
+      {playlists !== [] ? (
+        playlists.map((playlist, index) => (
+          <Link
+            key={index.toString()}
+            to={`/playlist/${playlist.title}#access_token=${
+              access_token
+            }`}
+          >
             <Playlist playlist={playlist} />
           </Link>
         ))
-        : <h1>No playlists yet...</h1>}
+      ) : (
+        <h1>No playlists yet...</h1>
+      )}
     </div>
   );
 };
